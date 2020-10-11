e5d228a28e68b38bdec1961d2983ee4eb2ad2331
Denis Korchuganov <DKorchuganov@yandex.ru>
2020-10-11 20:48:06 +0400

    rename2date: check the first argument
    Check if the first argument is an existing folder

--- rename2date.kts
+++ rename2date.kts
@@ -1,8 +1,17 @@
 import kotlin.system.exitProcess
+import java.nio.file.Files
+import java.nio.file.Paths
+
 
 if (args.size == 0) {
   println("Usage: rename2date <folder>")
   exitProcess(1)
+}
+
+val folder = Paths.get(args[0])
+if (!Files.isDirectory(folder)) {
+  println("This should be an existing folder: ${args[0]}")
+  exitProcess(2)
 }
 
 println(args[0])
