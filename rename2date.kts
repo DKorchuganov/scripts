9f653aa81f51e6089ff78a31832f140da1af8211
Denis Korchuganov <DKorchuganov@yandex.ru>
2020-10-18 13:38:00 +0400

    rename2date: added new name generation with time shift

--- rename2date.kts
+++ rename2date.kts
@@ -1,11 +1,13 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.attribute.BasicFileAttributes
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import kotlin.system.exitProcess
 
 
 if (args.size == 0) {
-  println("Usage: rename2date <folder>")
+  println("Usage: rename2date <folder> [<time shift in seconds>]")
   exitProcess(1)
 }
 
@@ -15,7 +17,18 @@ if (!Files.isDirectory(folder)) {
   exitProcess(2)
 }
 
+val timeShift = if (args.size > 1) {
+  args[1].toLong()
+} else 0
+
 Files.list(folder).forEach {
   val modified = Files.readAttributes(it, BasicFileAttributes::class.java).lastModifiedTime()
-  println("$it $modified")
+  val newName = "IMG_" +
+      modified.toInstant()
+          .plusSeconds(timeShift)
+          .atZone(ZoneId.systemDefault())
+          .toLocalDateTime()
+          .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + ".jpg"
+
+  println("$it $newName $modified")
 }
